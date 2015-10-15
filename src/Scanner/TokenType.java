981866d42e218352d70fe73424b410b0189e7273
user <user@e5>
2015-10-14 19:06:55 -0500

    Created a new Netbeans package Scanner and moved starter files for the scanner

---
+++ src/Scanner/TokenType.java
@@ -0,0 +1,19 @@
+package Scanner;
+
+
+
+
+/**
+ * Represents the various types of Tokens in mini pascal.
+ * @author steinmee
+ */
+public enum TokenType {
+    ID,
+    PROGRAM,
+    WHILE,
+    PLUS,
+    MINUS,
+    ASSIGN,
+    GREATER_THAN,
+    GREATER_THAN_EQUALS,
+}
