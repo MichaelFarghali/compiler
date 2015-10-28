219dc1a8b11b0f0b5ffbba61dcb9851fe63b3817
user <user@141.224.172.40>
2015-10-28 15:58:16 -0500

    Added in line comments and javadoc

--- src/Scanner/ScanTest.java
+++ src/Scanner/ScanTest.java
@@ -1,14 +1,12 @@
 package Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 import java.io.File;
 
 /**
- *
+ * The ScanTest class reads in a file with my Scanner class and prints to the 
+ * screen the tokens found and what token type they match to in the LookUpTable
+ 
  * @author Michael Farghali
  */
 public class ScanTest {
@@ -21,8 +19,8 @@ public class ScanTest {
         // Create a Scanner and point it at a file
         Scanner s = new Scanner(new File("input.txt"));
 
-        // Call nextToken() once
-        boolean thereIsAToken = true;
+        // Call nextToken() once to establish if there are any tokens
+        boolean thereIsAToken = s.nextToken();
 
         // While nextToken is true, print the tokens
         while (thereIsAToken) {
@@ -32,8 +30,7 @@ public class ScanTest {
             } else {
                 System.out.println("Didn't find token with lexeme " + s.getLexeme());
             }
-        }
-
-    }
-
-}
+            
+        } //end while
+    } //end main
+} //end ScanTest
