fc20fb42a32c2b4ce3abdb2c61689e2050560aff
user <user@e5>
2015-11-04 18:46:53 -0600

    Added test cases for nextToken()  in ScannerTest. Tests for valid and invalid symbols. Found two cases where the Scanner doesn't work properly. An example of incorrect processing is 10e++5 is returning as true when it should be false. Deleted some unnecessary files and altered test files for the test cases in ScannerTest class.

--- test/Scanner/ScannerTest.java
+++ test/Scanner/ScannerTest.java
@@ -11,6 +11,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.io.File;
+import java.io.IOException;
 
 /**
  *
@@ -43,26 +45,113 @@ public class ScannerTest {
     @Test
     public void testNextToken() {
         System.out.println("nextToken");
-        Scanner instance = null;
-        boolean expResult = false;
-        boolean result = instance.nextToken();
+        Scanner charTest = new Scanner(new File("inputBadCharTest.txt"));
+        Scanner numTest = new Scanner(new File("numbersTest.txt"));
+      
+        
+        // The following tests are for invalid characters %, #, and &
+        // from the inputBadCharTest.txt file using charTest instance of Scanner
+        
+        // Test that 'foo' is recognized
+        boolean expResult = true;
+        boolean result = charTest.nextToken();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        // Test that 'bar' is recognized token
+        expResult = true;
+        result = charTest.nextToken();
+        assertEquals(expResult, result);
+        // Test that '%' isn't valid
+        expResult = false;
+        result = charTest.nextToken();
+        assertEquals(expResult, result);
+        // Test that '#' is not valid
+        expResult = false;
+        result = charTest.nextToken();
+        assertEquals(expResult, result);
+        // Test that '&' isn't valid        
+        expResult = false;
+        result = charTest.nextToken();
+        assertEquals(expResult, result);
+        
+        // The following tests are too be used on the numbersTest.txt file
+        // using the numTest instance of Scanner class
+        
+        // Test that '1234' is valid
+        expResult = true;
+        result = numTest.nextToken();
+        assertEquals(expResult, result);
+        System.out.println(numTest.getLexeme());
+        
+        // Test that '7.123' is a valid token
+        expResult = true;
+        result = numTest.nextToken();
+        assertEquals(expResult, result);
+        System.out.println(numTest.getLexeme());
+        
+        // Test that '10e5' is valid token
+        expResult = true;
+        result = numTest.nextToken();
+        assertEquals(expResult, result); 
+        System.out.println(numTest.getLexeme());
+        
+        // Test that 10e+5 is valid token
+        expResult = true;
+        result = numTest.nextToken();
+        assertEquals(expResult, result); 
+        System.out.println(numTest.getLexeme());
+        
+        // Test that 10e-5 is valid
+        expResult = true;
+        result = numTest.nextToken();
+        assertEquals(expResult, result); 
+        System.out.println(numTest.getLexeme());
+        
+        // Test that '1.' is not valid
+        expResult = false;
+        result = numTest.nextToken();
+        assertEquals(expResult, result); 
+        System.out.println(numTest.getLexeme());
+        
+        // Test that 10e5e5 isn't valid token
+        expResult = false;
+        result = numTest.nextToken();
+        assertEquals(expResult, result); 
+        System.out.println(numTest.getLexeme());
+        
+        // Test that 10.123.456 is not valid
+        expResult = false;
+        result = numTest.nextToken();
+        assertEquals(expResult, result); 
+        System.out.println(numTest.getLexeme());
+        /*
+        // Test that 10e++5 is not valid
+        // Need to change code in nextToken because it's currently valid
+        expResult = true;
+        result = numTest.nextToken();
+        assertEquals(expResult, result); 
+        System.out.println(numTest.getLexeme());
+        
+        // Test that 10e--5 is not valid
+        // Need to change code. Currently works
+        expResult = false;
+        result = numTest.nextToken();
+        assertEquals(expResult, result); 
+        System.out.println(numTest.getLexeme());
+        */
     }
-
     /**
      * Test of getToken method, of class Scanner.
      */
     @Test
     public void testGetToken() {
         System.out.println("getToken");
-        Scanner instance = null;
-        TokenType expResult = null;
+        Scanner instance = new Scanner(new File("input.txt"));
+        TokenType expResult = TokenType.PROGRAM;
+        System.out.println(instance.getToken());
         TokenType result = instance.getToken();
+        
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
@@ -71,12 +160,15 @@ public class ScannerTest {
     @Test
     public void testGetLexeme() {
         System.out.println("getLexeme");
-        Scanner instance = null;
-        String expResult = "";
+        Scanner instance = new Scanner(new File("input.txt"));
+        instance.nextToken();
+        String expResult = "program";
+        
         String result = instance.getLexeme();
+        System.out.println(expResult);
+        System.out.println(result);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
-    
+   
 }
