bc22f1a68407937dab9aa3d1e171195f667e347c
user <user@e5>
2015-11-12 12:35:41 -0600

    Added Parser package and a ParseTest and Parser class

---
+++ src/Parser/ParseTest.java
@@ -0,0 +1,12 @@
+
+package Parser;
+
+/**
+ * The ParseTest class checks that Parse class is working as expected
+ * @Michael Farghali
+ */
+public class ParseTest {
+    
+    
+    
+}
---
+++ src/Parser/Parser.java
@@ -0,0 +1,16 @@
+
+package Parser;
+import Scanner.LookupTable;
+import Scanner.Scanner;
+
+/**
+ * The Parser class will eventually process a source file and create a parse tree
+ * or reject the source as invalid using recursive descent parsing.  
+ * @Michael Farghali
+ */
+public class Parser {
+    
+    private Scanner scanner;
+    
+    
+}
