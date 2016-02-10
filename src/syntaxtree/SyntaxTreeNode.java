8b85eb8908ede2441af64c567540408a9c5df763
Michael Farghali <farghalm@augsburg.edu>
2016-02-10 15:00:05 -0600

    Cleaned up directory, added starter files for ExpressionNode and SyntaxTreeNode

---
+++ src/syntaxtree/SyntaxTreeNode.java
@@ -0,0 +1,24 @@
+
+package syntaxtree;
+
+/**
+ *
+ * @author Michael Farghali
+ */
+public abstract class SyntaxTreeNode {
+    
+    
+     /**
+     * Creates a String representation of this node and its children.
+     * @param level The tree level at which this node resides.
+     * @return A String representing this node.
+     */
+    public String indentedToString( int level) {
+        String answer = "";
+        if( level > 0) {
+            answer = "|-- ";
+        }
+        for( int indent = 1; indent < level; indent++) answer += "--- ";
+        return( answer);
+    }    
+}
